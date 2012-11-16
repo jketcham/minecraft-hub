4308635f4e6bfbd4a88c1a5f7c74b00b4c90298e
Jack Ketcham <kcajketcham@gmail.com>
2012-11-16 00:07:38 -0700

    Complete conversion to SCSS

--- core/init.inc.php
+++ core/init.inc.php
@@ -4,12 +4,8 @@ session_start();
 
 $path = dirname(__FILE__);
 
-include("{$path}/inc/user.inc.php");
-
 include("{$path}/config.inc.php");
 
 include("{$path}/inc/mc.inc.php");
-
- 
 
 ?>
--- ext/config.rb
+++ ext/config.rb
@@ -1,3 +1,4 @@
 
-output_style = :compressed # by Compass.app 
 line_comments = false # by Compass.app 
+
+output_style = :compressed # by Compass.app 
